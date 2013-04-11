1418e62caadcfd62e1182b35022dc7cfe763ce1d
Eqric <eric9036@gmail.com>
2013-04-11 15:49:28 +0200

    test

--- OCR/src/Main.java
+++ OCR/src/Main.java
@@ -14,7 +14,7 @@ public class Main {
 	public static void main(String args[]) throws IOException{
 		int XLength = 0;
 		int YLength = 0;
-		
+		// Hej Marcus
 		ArrayList<VectorColor> testList = new ArrayList<VectorColor>();
 		Scanner scan = new Scanner(System.in);
 		System.out.print("File path:");
